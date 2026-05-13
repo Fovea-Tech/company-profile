82b4d7b5e67827d05d4e3a24eeecea50a06dc8c1
IfanDev <irfannurhadi616@gmail.com>
2026-05-14 00:32:40 +0700

    Update sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
     },
     // Jika Anda memiliki sitemap nanti, pastikan URL-nya benar
-    sitemap: 'https://www.fovea.digital/sitemap.xml',
+    sitemap: 'https://fovea.digital/sitemap.xml',
   };
 }
